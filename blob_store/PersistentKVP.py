f41f0ca5db226199b2f627ade67c4292b673314c
Rufelle Emmanuel Pactol <rufelleemmanuel.pactol@cit.edu>
2024-10-08 22:13:09 +0800

    changed to redis

--- blob_store/PersistentKVP.py
+++ blob_store/PersistentKVP.py
@@ -1,62 +1,63 @@
 import json
+import redis
 import os
 
 class PersistentKVP:
     def __init__(self, name):
-        self.filename = f"{name}.kvp"
-        self.data = {}
-        self._load()
+        self.name = name
+        redis_url = os.environ.get('REDIS_URL')
+        if not redis_url:
+            raise ValueError("REDIS_URL environment variable is not set")
+        self.redis_client = redis.from_url(redis_url)
 
     def __str__(self):
-        return json.dumps(self.data, indent=4)
-    def _load(self):
-        if os.path.exists(self.filename):
-            with open(self.filename, 'r') as f:
-                self.data = json.load(f)
-        else:
-            self._store()
-
-    def _store(self):
-        with open(self.filename, 'w') as f:
-            json.dump(self.data, f)
+        return json.dumps(self.redis_client.hgetall(self.name), indent=4)
 
     def __setitem__(self, key, value):
-        self.data[key] = value
-        self._store()
+        self.redis_client.hset(self.name, key, json.dumps(value))
 
     def persist(self):
-        self._store()
+        # Redis automatically persists data, so this method is not needed
+        pass
 
     def __getitem__(self, key):
-        return self.data[key]
+        value = self.redis_client.hget(self.name, key)
+        if value is None:
+            raise KeyError(key)
+        return json.loads(value)
 
     def __contains__(self, key):
-        return key in self.data
+        return self.redis_client.hexists(self.name, key)
 
     def __len__(self):
-        return len(self.data)
+        return self.redis_client.hlen(self.name)
 
     def __iter__(self):
-        return iter(self.data)
+        return iter(self.keys())
 
     def keys(self):
-        return self.data.keys()
+        return [key.decode() for key in self.redis_client.hkeys(self.name)]
 
     def values(self):
-        return self.data.values()
+        return [json.loads(value) for value in self.redis_client.hvals(self.name)]
 
     def items(self):
-        return self.data.items()
+        return [(key.decode(), json.loads(value)) for key, value in self.redis_client.hgetall(self.name).items()]
 
     def get(self, key, default=None):
-        return self.data.get(key, default)
+        value = self.redis_client.hget(self.name, key)
+        return json.loads(value) if value is not None else default
 
     def update(self, other=None, **kwargs):
         if other:
-            self.data.update(other)
-        self.data.update(kwargs)
-        self._store()
+            if hasattr(other, 'keys'):
+                for key in other.keys():
+                    self[key] = other[key]
+            else:
+                for key, value in other:
+                    self[key] = value
+        for key, value in kwargs.items():
+            self[key] = value
 
     def clear(self):
-        self.data.clear()
-        self._store()
+        pass
